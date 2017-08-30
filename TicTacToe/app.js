ba9e1bf2c5f76bbd826d0b90cd77f6b43c0b9f0a
TomerPacific <tomerpacific@gmail.com>
2017-08-30 15:54:24 +0300

    Add files via upload
    Added AI
    Refactored code
    Game works
    Only missing thing is option to choose to be X or O

--- TicTacToe/app.js
+++ TicTacToe/app.js
@@ -1,17 +1,21 @@
-var gameBoard = new Array(3);
-gameBoard[0] = [0,0,0];
-gameBoard[1] = [0,0,0];
-gameBoard[2] = [0,0,0];
-
+// Variables for MiniMax Algorithm
+var gameBoardForAI = ["0","1","2", "3", "4", "5", "6", "7", "8"];
+var aiPlayer = "O";
+var humanPlayer = "X";
 var svgNS = "http://www.w3.org/2000/svg";
 var whoIsPlaying = "Player";
 var mark = "X";
 var winner = 0;
 var playerWins = 0;
 var computerWins = 0;
+var mySvg;
+var amountOfMarks = 0;
+const X_POSITION_PLACEMENT_FACTOR = 65;
+const Y_POSITION_PLACEMENT_FACTOR = 130;
 
 window.onload = function()
 {
+	mySvg = document.getElementById("mySVG");
 	updateScore();
 }
 
@@ -23,140 +27,249 @@ function updateScore()
 
 function restartGame()
 {
-	gameBoard[0] = [0,0,0];
-	gameBoard[1] = [0,0,0];
-	gameBoard[2] = [0,0,0];
+	gameBoardForAI = ["0","1","2", "3", "4", "5", "6", "7", "8"];
 	winner = 0;
-	document.getElementById("mySVG").removeChild("myLine");
+	var childrenOfSvg = mySvg.childNodes;
+	for(var i = childrenOfSvg.length - 1; i > childrenOfSvg.length - amountOfMarks; i--)
+	{
+		var elem = childrenOfSvg[i];
+		if(elem.id === "5555")
+		{
+			mySvg.removeChild(childrenOfSvg[i]);
+		}
+	}
+	amountOfMarks = 0;
 }
 
-function handleMove(element, position)
+function handleMove(element, position, arrayIndex)
 {
 	var firstIndex = parseInt(position[0]);
 	var secondIndex = parseInt(position[1]);
 	var parent = element.parentElement;
 	var x = parseInt(parent.getAttribute("x"));
 	var y = parseInt(parent.getAttribute("y"));
-	if(gameBoard[firstIndex][secondIndex] >= 0)
+	createMark(x, y);
+	gameBoardForAI[arrayIndex] = mark;
+	checkIfWon();
+	turnOver();
+	if(whoIsPlaying === "Computer")
 	{
-		createMark(x, y);
-
-		if(whoIsPlaying === "Player")
-		{
-			gameBoard[firstIndex][secondIndex] = -1;
-		}
-		else
-		{
-			gameBoard[firstIndex][secondIndex] = -2;
-		}
-		checkIfWon();
-		if(winner != 0)
-		{
-			alert("Game Over! " + whoIsPlaying + " WON!");
-			if(whoIsPlaying === "Player")
-			{
-				playerWins++;
-			}
-			else
-			{
-				computerWins++;
-			}
-			updateScore();
-			restartGame();
-		}
-		else
-		{
-			turnOver(whoIsPlaying);
-		}
-		
+		handleComputerMove();
 	}
+	
+}
+
+function handleComputerMove()
+{
+		
+	var move = miniMax(gameBoardForAI, mark);
+	var moveTransposed = figureOutMark(move.index);
+	gameBoardForAI[move.index] = mark;
+	var elem = document.getElementById(move.index);
+	parent = elem.parentElement;
+	var x1 = parseInt(parent.getAttribute("x"));
+	var y1 = parseInt(parent.getAttribute("y"));
+	createMark(x1, y1);
+	checkIfWon();
+	turnOver();
+
+}
+
+
+function figureOutMark(index)
+{
+	var result;
+	//casting type to integer
+	switch(+index) 
+	{
+		case 0:
+			result = "00";
+			break;
+		case 1:
+			result = "01";
+			break;
+		case 2:
+			result = "02";
+			break;
+		case 3:
+			result = "10";
+			break;
+		case 4:
+			result = "11";
+			break;
+		case 5:
+			result = "12";
+			break;
+		case 6:
+			result = "20";
+			break;
+		case 7:
+			result = "21";
+			break;
+		case 8:
+			result = "22";
+			break;
+		default:
+	}
+	return result;
 }
 
 function createMark(first, second)
 {
+	var newText = document.createElementNS(svgNS,"text");
+	newText.setAttributeNS(null, "id", "5555");
+	newText.setAttributeNS(null,"x",first + X_POSITION_PLACEMENT_FACTOR);     
+	newText.setAttributeNS(null,"y",second + Y_POSITION_PLACEMENT_FACTOR); 
+	newText.setAttributeNS(null,"font-size","100");
 	if(mark === "X")
 	{
-		var myLine = document.createElementNS(svgNS,"line"); 
-		myLine.setAttributeNS(null,"id","myLine");
-	    myLine.setAttributeNS(null,"x1",first);
-	    myLine.setAttributeNS(null,"y1",second);
-	    myLine.setAttributeNS(null,"x2",first + 200);
-	    myLine.setAttributeNS(null,"y2",second + 200);
-	    myLine.setAttributeNS(null,"stroke","white");
-	    myLine.setAttributeNS(null,"stroke-width",2);
-
-	    document.getElementById("mySVG").appendChild(myLine);
-
-	    myLine = document.createElementNS(svgNS,"line"); 
-	    myLine.setAttributeNS(null,"id","myLine");
-	    myLine.setAttributeNS(null,"x1",first+200);
-	    myLine.setAttributeNS(null,"y1",second);
-	    myLine.setAttributeNS(null,"x2",first);
-	    myLine.setAttributeNS(null,"y2",second+200);
-	    myLine.setAttributeNS(null,"stroke","white");
-	    myLine.setAttributeNS(null,"stroke-width",2);
-		document.getElementById("mySVG").appendChild(myLine);
+		newText.innerHTML = "X";
 	}
 	else
 	{
-		var myCircle = document.createElementNS(svgNS,"circle"); 
-	    myCircle.setAttributeNS(null,"cx", first+100);
-	    myCircle.setAttributeNS(null,"cy", second+100);
-	    myCircle.setAttributeNS(null,"r",95);
-	    myCircle.setAttributeNS(null,"fill","none");
-	    myCircle.setAttributeNS(null,"stroke","white");
-
-	    document.getElementById("mySVG").appendChild(myCircle);
+		newText.innerHTML = "O";
 	}
-    
+	mySvg.appendChild(newText);
+    amountOfMarks++;
 }
 
-function turnOver(currentPlayer)
+function turnOver()
 {
-	if(currentPlayer === "Player")
-	{
-		whoIsPlaying = "Computer";
-		mark = "O";
-	}
-
-	else
-	{
-		whoIsPlaying = "Player";
-		mark = "X";
-	}
+	whoIsPlaying = whoIsPlaying === "Player" ? "Computer" : "Player";
+	mark = whoIsPlaying === "Player" ? humanPlayer : aiPlayer;
 }
 
 function checkIfWon()
 {
-	var found = false;
-	for(var i = 0; i < gameBoard.length && !found; i++)
-	{
-		//check if there are 3 identical symbols across a row
-		if((gameBoard[i][0] === gameBoard[i][1] && gameBoard[i][1] === gameBoard[i][2] && gameBoard[i][0] != 0) ||
-			gameBoard[0][i] === gameBoard[1][i] && gameBoard[1][i] === gameBoard[2][i] && gameBoard[0][i] != 0)
-		{
-			winner = gameBoard[i][0];
-			found = true;
-		}
-	}
+	var isThereAWin = winning(gameBoardForAI, whoIsPlaying);
 
-	if(!found)
+	if(isThereAWin)
 	{
-		if((gameBoard[0][0] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][2] && gameBoard[0][0] != 0) ||
-		   gameBoard[0][2] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][0] && gameBoard[0][2] != 0)
-		{
-			winner = gameBoard[0][0];
-		}
+		winner = whoIsPlaying;
 	}
-
+	if(winner !== 0 || amountOfMarks === 9)
+	{
+		gameOver();
+	}
 }
 
-function printBoard()
+function gameOver()
 {
-	for(var i = 0; i < gameBoard.length; i++)
+	if(winner != 0)
 	{
-		console.log("gameboard["+i+"][0]" + gameBoard[i][0]);
-		console.log("gameboard["+i+"][1]" + gameBoard[i][1]);
-		console.log("gameboard["+i+"][2]" + gameBoard[i][2]);
+		alert("Game Over! " + whoIsPlaying + " WON!");
+		if(whoIsPlaying === "Player")
+		{
+			playerWins++;
+		}
+		else
+		{
+			computerWins++;
+		}
 	}
+	else
+	{
+		alert("Game Over! It's a DRAW");
+	}
+
+	updateScore();
+	restartGame();
+}
+
+function freeSpotsInGameBoard(board)
+{
+	var results = [];
+	for(var i  = 0; i < board.length; i++)
+	{
+		if(parseInt(board[i]) === i)
+		{
+			results.push(i);
+		}
+	}
+
+	return results;
+}
+
+function miniMax(newBoard, player)
+{
+	var availableSpots = freeSpotsInGameBoard(newBoard);
+
+	if(winning(newBoard, humanPlayer))
+	{
+		return {score : -10};
+	}
+	else if(winning(newBoard, aiPlayer))
+	{
+		return {score: +10};
+	}
+	else if(availableSpots.length === 0)
+	{
+		return {score : 0};
+	}
+
+	var moves = [];
+
+	for(var i = 0; i < availableSpots.length; i++)
+	{
+		var move = {};
+		move.index = newBoard[availableSpots[i]];
+	
+		newBoard[availableSpots[i]] = player;
+
+		if(player === aiPlayer)
+		{
+			var result = miniMax(newBoard, humanPlayer);
+			move.score = result.score;
+		}
+		else{
+			var result = miniMax(newBoard, aiPlayer);
+			move.score = result.score;
+		}
+
+		newBoard[availableSpots[i]] = move.index;
+		moves.push(move);
+	}
+
+	var bestMove;
+	if(player === aiPlayer)
+	{
+		var bestScore = -10000;
+		for(var i = 0; i < moves.length; i++)
+		{
+			if(moves[i].score > bestScore)
+			{
+				bestScore = moves[i].score;
+				bestMove = i;
+			}
+		}
+	}
+	else
+	{
+		var bestScore = 10000;
+	    for(var i = 0; i < moves.length; i++){
+	      if(moves[i].score < bestScore){
+		        bestScore = moves[i].score;
+		        bestMove = i;
+	      	}
+   		}
+	}
+
+	return moves[bestMove];
+}
+
+function winning(board, player){
+ if (
+ (board[0] == player && board[1] == player && board[2] == player) ||
+ (board[3] == player && board[4] == player && board[5] == player) ||
+ (board[6] == player && board[7] == player && board[8] == player) ||
+ (board[0] == player && board[3] == player && board[6] == player) ||
+ (board[1] == player && board[4] == player && board[7] == player) ||
+ (board[2] == player && board[5] == player && board[8] == player) ||
+ (board[0] == player && board[4] == player && board[8] == player) ||
+ (board[2] == player && board[4] == player && board[6] == player)
+ ) {
+ 	return true;
+ } else {
+ 	return false;
+ }
 }
