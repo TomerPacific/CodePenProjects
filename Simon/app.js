c9d762fc6e2fd06feb5d8477b666a4fe19af6192
TomerPacific <tomerpacific@gmail.com>
2017-09-19 14:17:26 +0300

    Add files via upload
    Fixed display and functionality when turning game on/off

--- Simon/app.js
+++ Simon/app.js
@@ -4,10 +4,9 @@ var gameStarted = false;
 var strictModeBtn;
 var startGameBtn;
 var numOfMoves;
+var moves = 1;
 
-var btnsPlayRoutine = [
-	['red', 'blue', 'green', 'yellow', 'green', 'blue', 'blue', 'red', 'red'],
-];
+var btnsPlayRoutine = ['red', 'blue', 'green', 'yellow', 'green', 'blue', 'blue', 'red', 'red'];
 
 var simonSoundRed = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
 var simonSoundBlue = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
@@ -34,32 +33,52 @@ window.onload = function()
 	 	if(machineOn && !gameStarted)
 	  	{
 	  		gameStarted = true;
-	  		startPlaying(btnsPlayRoutine[0], 0);
+	  		numOfMoves.innerHTML = moves.toString();
+			for(var i = 0; i < moves; i++){
+				startPlaying(i);
+			}
 	  	}
 	});
 }
 
-function startPlaying(routine){
+function startPlaying(index){
 
-	var whatToPlay = document.getElementById(routine[0].toString());
-	switch(whatToPlay.id){
+		var whatToPlay = document.getElementById(btnsPlayRoutine[index].toString());
+		switch(whatToPlay.id){
 		case 'red':
 			simonSoundRed.play();
 			whatToPlay.style.background = 'rgba(255,0,0,1)';
+			setTimeout(function(){
+				whatToPlay.style.background = 'rgba(255,0,0,0.6)';
+			},500);
+			
 			break;
 		case 'blue':
 			simonSoundBlue.play();
+			whatToPlay.style.background = 'rgba(255,0,0,1)';
+			setTimeout(function(){
+				whatToPlay.style.background = 'rgba(255,0,0,0.6)';
+			},500);
 			break;
 		case 'green':
 			simonSoundGreen.play();
+			whatToPlay.style.background = 'rgba(255,0,0,1)';
+			setTimeout(function(){
+				whatToPlay.style.background = 'rgba(255,0,0,0.6)';
+			},500);
 			break;
 		case 'yellow':
 			simonSoundYellow.play();
+			whatToPlay.style.background = 'rgba(255,0,0,1)';
+			setTimeout(function(){
+				whatToPlay.style.background = 'rgba(255,0,0,0.6)';
+			},500);
 			break;
 		default:
 			console.log("Error in button id");
 			break;
-	}
+		}
+	
 }
 
 $(document).ready(function() {
@@ -73,6 +92,8 @@ $(document).ready(function() {
   	{
   		$(this).animate({"left" : "-=25px"}, "slow");
   		numOfMoves.style.fill="black";
+  		gameStarted = false;
+  		numOfMoves.innerHTML = "0";
   	}
     machineOn = !machineOn;
     
@@ -81,7 +102,23 @@ $(document).ready(function() {
 
 function handleClick(btnId){
 	if(machineOn && gameStarted){
-
+	switch(btnId){
+		case 'red':
+			simonSoundRed.play();
+			break;
+		case 'blue':
+			simonSoundBlue.play();
+			break;
+		case 'green':
+			simonSoundGreen.play();
+			break;
+		case 'yellow':
+			simonSoundYellow.play();
+			break;
+		default:
+			console.log("Error in button id " + btnId);
+			break;
+		}	
 	}
 
 }
