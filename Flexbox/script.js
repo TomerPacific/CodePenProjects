c86b5408a7b04b20204ae662dc0a99d444980805
Tomer Ben Rachel <tomerpacific@gmail.com>
2017-12-02 18:32:31 +0200

    CodePen Projects - Flexbox
    Updated way to handle classes using classList
    Added method to remove same name classes (justify, direction)

--- Flexbox/script.js
+++ Flexbox/script.js
@@ -2,21 +2,46 @@ const btns = document.querySelectorAll('.flexBtn');
 
 let userInputDiv = document.querySelector('#userInput');
 
+function checkSimilar(classList, classToBeAdded){
+
+  let startOfClass = classToBeAdded.split('-');
+  for(let i = 0; i < classList.length; i++){
+    if(classList[i].split('-')[0] === startOfClass[0]){
+      classList.remove(classList[i]);
+    }
+  }
+  return classList;
+}
+
 function btnPressed(event){
   event.preventDefault();
   const id = this.dataset.btnType;
-  const classes = userInputDiv.className.split(' ');
-  let classAlreadyInUse = false;
-  for(const key in classes){
-  	if(classes[key] === id){
-  		classAlreadyInUse = true;
-  		break;
-  	}
+  let classList = userInputDiv.classList;
+  classList = checkSimilar(classList, id);
+  if(!classList.contains(id)){
+    classList.add(id);
   }
-  if(!classAlreadyInUse)
-  {
-  	userInputDiv.className += " " + id;
-  }
+
+
+  //const classes = userInputDiv.className.split(' ');
+  // let classAlreadyInUse = false;
+  // for(const key in classes){
+
+  //   if(checkIfSimilar(classes, key, id)){
+  //     classAlreadyInUse = true;
+  //     break;
+  //   }
+
+  // 	if(classes[key] === id){
+  // 		classAlreadyInUse = true;
+  // 		break;
+  // 	}
+  // }
+
+  // if(!classAlreadyInUse)
+  // {
+  // 	userInputDiv.className += " " + id;
+  // }
 }
 
 btns.forEach(btn => {
