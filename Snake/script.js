bed89360c3bbce9392addaaf3ad186837f5c6838
Tomer Ben Rachel <tomerpacific@gmail.com>
2018-07-06 15:34:30 +0200

    CodePenProjects Snake - Adding logic to draw the snake

--- Snake/script.js
+++ Snake/script.js
@@ -1,6 +1,8 @@
 const CANVAS_BORDER_COLOR = 'black';
 const CANVAS_BACKGROUND_COLOR = 'white';
 
+let context;
+
 let snake = [
 	{x: 150, y: 150},
 	{x: 140, y: 150},
@@ -13,14 +15,25 @@ let snake = [
 
 function init() {
 	var canvas = document.querySelector('#game');
-	var context = canvas.getContext('2d');
+	context = canvas.getContext('2d');
 
 	context.fillStyle = CANVAS_BACKGROUND_COLOR;
 	context.strokeStyle = CANVAS_BORDER_COLOR;
 
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	context.strokeRect(0, 0, canvas.width, canvas.height);
+
+	snake.forEach(drawSnake);
 }
+
+function drawSnake(snakeChain) {
+	context.fillStyle = 'lightgreen';
+	context.strokeStyle = 'darkgreen';
+
+	context.fillRect(snakeChain.x, snakeChain.y, 10, 10);
+	context.strokeRect(snakeChain.x, snakeChain.y, 10, 10);
+}
+
 
 
 init();
