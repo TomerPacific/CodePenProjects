72d04037b7255d06731c0777ce3f016effefcf74
Tomer Ben Rachel <tomerpacific@gmail.com>
2019-03-12 20:02:44 +0200

    CodePenProjects CSSAnimations adding logic to handle different attributes

--- CSSAnimations/script.js
+++ CSSAnimations/script.js
@@ -2,5 +2,11 @@ let animate = document.getElementById("animationBlock");
 
 function setInput(id) {
   let inputSource = document.getElementById(id);
-  animate.style.animationDuration = inputSource.value + "s";
+   if (id === "duration") {
+    animate.style.animationDuration = inputSource.value + "s";
+  } else if (id === "count") {
+    animate.style.animationIterationCount = inputSource.value;
+  } else if (id === "delay") {
+     animate.style.animationDelay = inputSource.value + "s";
+  }
 }
