a66c203f70b85244f60fe11b18d0dce374f894bc
Tomer Ben Rachel <tomerpacific@gmail.com>
2019-04-04 20:21:28 +0300

    CodePenProjects Typewriter fixing keyCode and adding audio for return

--- Typewriter/script.js
+++ Typewriter/script.js
@@ -1,15 +1,18 @@
 let inputElement = document.getElementById("userInput");
-
+let audio;
 inputElement.focus();
 
 
-document.onkeypress = function (e) {
+document.onkeydown = function (e) {
     e = e || window.event;
   
    //enter pressed
-   if (e.keycode === 13) {
-     inputElement.style.height += '100px';
+   if (e.keyCode === 13) {
+   	audio = new Audio("typewriter-paper-1.mp3");
+    inputElement.style.height += '100px';
+   } else {
+   	 audio = new Audio("typewriter-key-1.mp3");
    }
-    var audio = new Audio("typewriter-key-1.mp3");
-    audio.play();
+   
+   audio.play();
 };
