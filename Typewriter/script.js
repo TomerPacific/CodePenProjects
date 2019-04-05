80e966de820ade50c6a02ee4ae35f20287d0d162
Tomer Ben Rachel <tomerpacific@gmail.com>
2019-04-05 10:29:00 +0300

    CodePenProjects Typewriter changing sound for enter key, disabling backspace and delete keys and adding constants

--- Typewriter/script.js
+++ Typewriter/script.js
@@ -1,5 +1,10 @@
 let inputElement = document.getElementById("userInput");
 let audio;
+
+const BACKSPACE = 8;
+const DELETE = 46;
+const ENTER = 13;
+
 inputElement.focus();
 
 
@@ -7,9 +12,11 @@ document.onkeydown = function (e) {
     e = e || window.event;
   
    //enter pressed
-   if (e.keyCode === 13) {
-   	audio = new Audio("typewriter-paper-1.mp3");
+   if (e.keyCode === ENTER) {
+   	audio = new Audio("typewriter-forward-1.mp3");
     inputElement.style.height += '100px';
+   } else if (e.keyCode === BACKSPACE || event.keyCode == DELETE) {
+	 return false;
    } else {
    	 audio = new Audio("typewriter-key-1.mp3");
    }
