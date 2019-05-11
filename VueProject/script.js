62e617954af15b1042c919716f35bd9f07775098
unknown <tomerpacific@gmail.com>
2019-05-11 09:43:15 +0300

    CodePenProjects VueProject refactoring quantity and title logic into computed methods

--- VueProject/script.js
+++ VueProject/script.js
@@ -1,25 +1,25 @@
-var product = "Socks";
-
 var app = new Vue({
 	el: '#app', //Connects to the div with the id app
 	data: {
+		brand: "Vue",
 		product: "Socks",
 		description: "A pair of warm, fuzzy socks",
-		image: "./assets/socks-green.png",
+		selectedVariant: 0,
 		altText: "A pair of socks",
-		inventory: 100,
 		onSale: true,
 		details: ["80% cotton", "20% polyester"],
 		variants: [
 			{
 				variantId: 2234,
 				variantColor: "green",
-				variantImage: "./assets/socks-green.png"
+				variantImage: "./assets/socks-green.png",
+				variantQuantity: 10
 			},
 			{
 				variantId: 2235,
 				variantColor: "blue",
-				variantImage: "./assets/socks-gray.png"
+				variantImage: "./assets/socks-gray.png",
+				variantQuantity: 0
 			}
 		],
 		cart: 0
@@ -28,8 +28,20 @@ var app = new Vue({
 		addToCart: function() {
 			this.cart += 1
 		},
-		changeImage: function(vImage) {
-			this.image = vImage;
+		changeImage: function(index) {
+			this.selectedVariant = index;
+		}
+	},
+	computed: {
+		title() {
+			return this.brand + ' ' + this.product
+		},
+		image() {
+			let img = this.variants[this.selectedVariant].variantImage
+			console.log(img);
+		},
+		inStock() {
+			return this.variants[this.selectedVariant].variantQuantity
 		}
 	}		
 });
