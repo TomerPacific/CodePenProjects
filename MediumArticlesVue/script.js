671a3913db0e8b5e6c5d90d27f7f556fdb4627f6
unknown <tomerpacific@gmail.com>
2019-05-25 22:04:27 +0300

    CodePenProjects MediumArticlesVue adding logic to present articles from data

--- MediumArticlesVue/script.js
+++ MediumArticlesVue/script.js
@@ -1,7 +1,21 @@
 var app = new Vue({
 	el: '#app', //Connects to the div with the id app
 	data: {
-		title: 'Medium Articles Viewer - Now with Vue.js!'
+		title: 'Medium Articles Viewer - Now with Vue.js!',
+		articles: [
+			{
+				name: 'Android Layouts',
+				link: 'https://medium.freecodecamp.org/how-to-make-sense-of-the-many-android-layouts-693b262706e0',
+				publish_date: 'June 2018',
+				background_image: 'https://github.com/TomerPacific/MediumArticles/blob/master/images/AndroidLayouts.jpg?raw=true'
+			},
+			{
+				name: 'Android View Visibility',
+				link: 'https://medium.freecodecamp.org/how-and-why-to-use-android-visibility-listeners-971e3b6511ec',
+				publish_date: 'July 2018',
+				background_image: 'https://github.com/TomerPacific/MediumArticles/blob/master/images/AndroidVisibility.jpg?raw=true'
+			}
+		]
 	},
 	methods: {
 		
