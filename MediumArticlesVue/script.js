0b9e8a9e02ca87053b6928a79771bc68abb07b82
unknown <tomerpacific@gmail.com>
2019-06-01 01:04:26 +0300

    CodePenProjects MediumArticlesVue adding two more articles

--- MediumArticlesVue/script.js
+++ MediumArticlesVue/script.js
@@ -36,15 +36,25 @@ var app = new Vue({
 				name: 'Android Layouts',
 				link: 'https://medium.freecodecamp.org/how-to-make-sense-of-the-many-android-layouts-693b262706e0',
 				publish_date: 'June 2018',
-				background_image: 'https://github.com/TomerPacific/MediumArticles/blob/master/images/AndroidLayouts.jpg?raw=true',
-				class: 'android_layouts'
+				background_image: 'https://github.com/TomerPacific/MediumArticles/blob/master/images/AndroidLayouts.jpg?raw=true'
 			},
 			{
 				name: 'Android View Visibility',
 				link: 'https://medium.freecodecamp.org/how-and-why-to-use-android-visibility-listeners-971e3b6511ec',
 				publish_date: 'July 2018',
-				background_image: 'https://github.com/TomerPacific/MediumArticles/blob/master/images/AndroidVisibility.jpg?raw=true',
-				class: 'android_view_visibility'
+				background_image: 'https://github.com/TomerPacific/MediumArticles/blob/master/images/AndroidVisibility.jpg?raw=true'
+			},
+			{
+				name: 'Chrome API',
+				link: 'https://medium.freecodecamp.org/features-of-the-chrome-api-you-should-know-bf5c8b6c7733',
+				publish_date: 'July 2018',
+				background_image: 'https://github.com/TomerPacific/MediumArticles/blob/master/images/ChromeAPI.jpg?raw=true'
+			},
+			{
+				name: 'How To Publish A Chrome Extension',
+				link: 'https://medium.freecodecamp.org/how-to-publish-your-chrome-extension-dd8400a3d53',
+				publish_date: 'July 2018',
+				background_image: 'https://github.com/TomerPacific/MediumArticles/blob/master/images/howToPublishChromeExtension.jpg?raw=true'
 			}
 		]
 	},
