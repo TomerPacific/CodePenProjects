bd9d92f6afe4181e0a417e24dee04e221a18d2ba
unknown <tomerpacific@gmail.com>
2019-05-31 18:39:20 +0300

    CodePenProjects MediumArticlesVue passing other parameters to article component and adding style rules

--- MediumArticlesVue/script.js
+++ MediumArticlesVue/script.js
@@ -7,16 +7,22 @@ Vue.component('medium-article', {
 		publish_date: {
 			type: String,
 			required: true
+		},
+		url: {
+			type: String,
+			required: true
 		}
 	},
-	template: `<a href="#" target="_blank">{{ name }}</a>`,
+	template: `<a v-bind:href="pageUrl" target="_blank">{{ name }}</a>`,
 	data() {
 		return {
 			article_name: this.name
 		}
 	},
 	computed: {
-		
+		pageUrl() {
+			return this.url
+		}
 	}
 })
 
@@ -30,13 +36,15 @@ var app = new Vue({
 				name: 'Android Layouts',
 				link: 'https://medium.freecodecamp.org/how-to-make-sense-of-the-many-android-layouts-693b262706e0',
 				publish_date: 'June 2018',
-				background_image: 'https://github.com/TomerPacific/MediumArticles/blob/master/images/AndroidLayouts.jpg?raw=true'
+				background_image: 'https://github.com/TomerPacific/MediumArticles/blob/master/images/AndroidLayouts.jpg?raw=true',
+				class: 'android_layouts'
 			},
 			{
 				name: 'Android View Visibility',
 				link: 'https://medium.freecodecamp.org/how-and-why-to-use-android-visibility-listeners-971e3b6511ec',
 				publish_date: 'July 2018',
-				background_image: 'https://github.com/TomerPacific/MediumArticles/blob/master/images/AndroidVisibility.jpg?raw=true'
+				background_image: 'https://github.com/TomerPacific/MediumArticles/blob/master/images/AndroidVisibility.jpg?raw=true',
+				class: 'android_view_visibility'
 			}
 		]
 	},
