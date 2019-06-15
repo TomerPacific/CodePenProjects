3008da7e5e5166be9ad6b39050bbe959cf270732
unknown <tomerpacific@gmail.com>
2019-06-15 19:10:37 +0300

    CodePenProjecs VueCounter finishing project by changing template of component

--- VueCounter/script.js
+++ VueCounter/script.js
@@ -1,26 +1,24 @@
-Vue.component('counter-btn', {
-	props: {
-		text: {
-			type: String,
-			required: true
-		}
-	},
-	template: `<button> {{ text }} </button>`,
-	data() {
-		return {
-
-		}
-	}
-})
-
-
 var app = new Vue({
 	el: '#app', //Connects to the div with the id app
 	data: {
 		title: 'Vue Counter',
-    counter: 0
+    	counter: 0
 	},
+	template: `<div id="main">
+				<h1> {{title}} </h1>
+				<p id="counter"> {{ counter }}</p>
+				<button @click="updateValue(+1)" id="inc">+1</button>
+				<button @click="updateValue(-1)" id="dec">-1</button>
+				<button @click="updateValue(0)" id="reset">Reset</button>
+				</div>`,
 	methods: {
-		
+		 updateValue: function (value) {
+		 	if (value === 0) {
+		 		this.counter = 0
+		 	} else {
+	     		this.counter += value
+		 	}
+ 
+    	}
 	}
 })
